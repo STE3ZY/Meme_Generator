47360557e1685750f3a2142db5df7a5fdcf4ff6e
steezy <karadimos.giannis@gmail.com>
2023-06-17 20:11:57 +0300

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css";
+import logo from "../images/troll-face.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={logo} className="header--image" alt="troll face" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
+}
